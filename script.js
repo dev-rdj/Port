ab400cb951b834c2f19182e7f796bfa68b2b9c89
dev Jeff <138620598+dev-rdj@users.noreply.github.com>
2026-04-27 01:06:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+gsap.registerPlugin(ScrollTrigger);
 
+/* HERO TEXT REVEAL */
+gsap.from(".hero h1", {
+  opacity:0,
+  y:50,
+  duration:1
+});
+
+/* PANEL SCROLL ANIMATION */
+gsap.utils.toArray(".panel").forEach(panel => {
+  gsap.from(panel, {
+    opacity:0,
+    y:100,
+    duration:1,
+    scrollTrigger:{
+      trigger:panel,
+      start:"top 80%",
+    }
+  });
+});
+
+/* SERVICES ANIMATION */
+gsap.from(".card", {
+  scrollTrigger:{
+    trigger:".services",
+    start:"top 80%"
+  },
+  opacity:0,
+  y:50,
+  stagger:0.2
+});
+
+/* PARALLAX HERO */
+gsap.to(".hero", {
+  scale:1.1,
+  scrollTrigger:{
+    trigger:".hero",
+    scrub:true
+  }
+});
